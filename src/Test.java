9281211e4d7af1c2dd2a31487085652d6196b6c7
Kimo <kimorahmani@gmail.com>
2017-10-23 17:04:09 +0200

    Test

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+
+public class Test extends Test2{
+
+	public static void main(String [] args)
+	{
+		new Test();
+	}
+  public Test(){
+	  run();
+  }
+	public void run(){
+		System.out.println("Hi");
+		System.out.println(str());
+	}
+}
